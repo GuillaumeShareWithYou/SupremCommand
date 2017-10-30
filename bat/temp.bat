84352cd6110cd55ff0ca5b47ce9ef489e3a0e578
Guillaume Mathieu <mathieu.guillaume93@gmail.com>
2017-10-30 14:02:45 +0100

    ajout de !git

--- bat/temp.bat
+++ bat/temp.bat
@@ -1,1 +1,3 @@
-git add *
+git add * 
+ git commit -m "ajout de !git " 
+ git push origin master
--- src/Engine/Git_command.java
+++ src/Engine/Git_command.java
@@ -1,4 +1,14 @@
 package Engine;
 
-public class Git_command {
+public class Git_command extends Command {
+    public Git_command(App app, String command) {
+        super(app, command);
+        if(stopCommand) return;
+
+        if(this.getOptions().contains("commit") && this.getArgs().size()>0)
+        {
+            String gitCommand = "!git add * && git commit -m \""+this.argsToString()+"\" && git push origin master";
+            new Windows_command(getApp(), gitCommand);
+        }
+    }
 }
